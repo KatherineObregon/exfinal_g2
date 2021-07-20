26d0b038037cd11992a20cd9c95924fbdb0cbb31
José García <garcia.josea@pucp.edu.pe>
2021-07-20 12:15:25 -0500

    -

--- exfinal_g2.js
+++ exfinal_g2.js
@@ -18,3 +18,67 @@ app.get("/principal", function (req, res) {
     res.sendFile(__dirname + "/principal.html");
 });
 
+socketio.on("connection", function (webSocket) {
+
+    console.log("usuario conectado c:")
+
+    usuariosConectados = usuariosConectados + 1;
+    socketio.emit("cantConect", usuariosConectados);
+
+    //mando historial de chat
+    var query = "select * from messages "
+        + "where date>(date_add(now(), interval -5 minute))";
+    conn.query(query, function (error, data) {
+        if (error) throw error;
+        webSocket.emit("historial de chat", data);
+    });
+
+    var nombreUsuario = "desconocido";
+    webSocket.on("establecer nombre de usuario", function (userName) {
+        nombreUsuario = userName;
+        listaUsuarios[listaUsuarios.length] = nombreUsuario;
+        socketio.emit("listaUsuarios", listaUsuarios);
+    });
+
+    webSocket.on("mensaje de chat", function (msg) {
+        //webSocket.emit("mostrar en chat",msg);
+        //socketio.emit("mostrar en chat", msg);
+        webSocket.broadcast.emit("mostrar en chat", nombreUsuario + ": " + msg);
+        var query = "insert into messages(user, message, date) "
+            + "values (?, ?,now())";
+        var params = [nombreUsuario, msg];
+        conn.query(query, params, function(error, data){
+            if(error) throw error;
+        });
+    });
+
+    webSocket.on("disconnect", function () {
+        console.log("usario desconectado "
+            + ":c\n------------------------------------");
+        usuariosConectados = usuariosConectados - 1;
+        socketio.emit("cantConect", usuariosConectados);
+
+        //borrar al usuario
+        listaUsuarios.forEach(function (usuario, index) {
+            if (usuario === nombreUsuario) {
+                listaUsuarios.splice(index, 1);
+            }
+        });
+
+        socketio.emit("listaUsuarios", listaUsuarios);
+    });
+
+    webSocket.on("typing", function (typing) {
+        var msg = {
+            usuario: nombreUsuario,
+            typing: typing
+        }
+        webSocket.broadcast.emit("typing", msg);
+    });
+
+    //cerrar sesión:
+    webSocket.on("cerrar sesion", function(){
+        webSocket.disconnect();
+    });
+})
+
